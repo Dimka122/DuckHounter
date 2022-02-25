f2d503734dfd35fbb59c207752f268cc7a5849e9
Dimka122 <kassindima1@gmail.com>
2022-02-25 15:43:43 +0300

    \\

--- Duck Hunt/Duck Hunt/Form1.Designer.cs
+++ Duck Hunt/Duck Hunt/Form1.Designer.cs
@@ -1,4 +1,16 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+
 namespace Duck_Hunt
 {
     partial class Form1
@@ -40,6 +52,51 @@ namespace Duck_Hunt
             this.Text = "Duck Hunt";
             this.ResumeLayout(false);
 
+            duck_position.X = 350;
+            duck_position.Y = 350;
+            duck_position.Width = 50;
+            duck_position.Height = 50;
+
+            this.DoubleBuffered = true;
+            this.Paint += Form1_Paint;
+            this.MouseMove += Form1_MouseMove;
+            this.Cursor.Dispose();
+            
+
+        }
+
+        Point mouse_location = new Point();
+        Rectangle duck_position = new Rectangle();
+        bool isAlive = true;
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            mouse_location.X = e.Location.X;
+            mouse_location.Y = e.Location.Y;
+
+            this.Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Flush();
+            GC.Collect();
+            if (isAlive)
+            {
+                e.Graphics.DrawImage(Image.FromFile("Duck1.png"), duck_position);
+            }
+            else
+            {
+                e.Graphics.DrawImage(Image.FromFile("Duck.png"), duck_position);
+            }
+            if(duck_position.IntersectsWith(new Rectangle(mouse_location,new Size(50,50))))
+            {
+                MessageBox.Show("Kill duck");
+                isAlive = false;
+            }
+
+            e.Graphics.DrawEllipse(Pens.Black, new RectangleF(mouse_location, new SizeF(30, 30)));
+            
         }
 
         #endregion
